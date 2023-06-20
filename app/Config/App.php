bdc651541d160eabf8db3a99123653dbbf18c265
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2023-06-20 22:34:06 +0700

    Update App.php
    update base url

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost/online_auction_api/';
+    public string $baseURL = 'http://localhost/ci4_online_auction_api/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
